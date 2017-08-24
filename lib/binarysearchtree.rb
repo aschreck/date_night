1b1ee9921442c7003017b10fe07c939152cdfd59
Aaron Schreck <schreck.aaron@gmail.com>
2017-08-23 20:24:07 -0600

    "Added files"

---
+++ lib/binarysearchtree.rb
@@ -0,0 +1,97 @@
+require_relative '../lib/node.rb'
+
+class BinarySearchTree
+  attr_accessor :root, :score, :name
+
+  def initialize
+    @root = nil
+  end
+
+  def insert (score,name)
+    #Search Tree looks for leading node
+    if @root.nil?
+      @root = Node.new(score,name, 0)
+      return 0 #root depth is always zero
+    else
+      #If it exists, Binary's insert delegates to Node's insert
+      return @root.insert(score, name)
+    end
+  end
+
+  def include?(query_score)
+
+    return false if @root.nil?
+
+    node = @root.find_node(query_score)
+
+    if node.nil?
+      return false
+    else
+      return true
+    end
+  end
+
+
+  def depth_of(query_score)
+    #return the depth of the node in question by score.
+    return nil if @root.nil?
+    node = @root.find_node(query_score)
+    return node.depth
+  end
+
+  def max
+    node = @root.node_extrema(:right)
+    return node.hash_converter
+  end
+
+  def min
+    node = @root.node_extrema(:left)
+    return node.hash_converter
+  end
+
+  def sort(node = @root)
+    #iterate through nodes and put them into an array
+    sorted_list =[]
+    return nil if node.nil?
+    sorted_list.push(sort(node.left))
+    sorted_list.push(node.hash_converter)
+    sorted_list.push(sort(node.right))
+
+    return sorted_list.flatten.compact
+  end
+
+  def load(file_name)
+    file = File.open(file_name, 'r')
+    i = 0
+    text = file.each do |line|
+      trimmed_line =line.tr(',', '')
+      line_array = trimmed_line.split(' ')
+      #line array[0] is the value and [1] is the name.
+      self.insert(line_array[0], line_array[1])
+      i += 1
+    end
+
+    return i
+
+  end
+
+  def health(query_depth)
+    count = @root.child_count
+    #nodes_at_depth should return an array of all the nodes with the first two fields.
+    nodes_at_depth = @root.find_nodes_at_depth(query_depth)
+    #here children is reporting 2
+    #take the existing array and add third element, children/count
+
+    health_arrays = nodes_at_depth.map do |node_array|
+      parentage_percent = 100.0 * (node_array[1]/count.to_f)
+      parentage_percent = parentage_percent.floor
+      node_array.push(parentage_percent)
+    end
+    return health_arrays
+  end
+
+end
+
+
+
+#guard clause: stop recuring if reach the end of whatever.
---
+++ tests/binarysearchtree_test.rb
@@ -0,0 +1,118 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/binarysearchtree'
+require_relative '../lib/node'
+require 'pry'
+
+class BinarySearchTreeTest < Minitest::Test
+
+  def prefilled_tree
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    tree.insert(16, "Johnny English")
+    tree.insert(92, "Sharknado 3")
+    tree.insert(50, "Hannibal Buress: Animal Furnace")
+    return tree
+  end
+
+  def test_it_exists
+    tree = BinarySearchTree.new
+    assert_instance_of BinarySearchTree, tree
+  end
+
+  def test_start_with_nil
+    tree = BinarySearchTree.new
+    assert_nil tree.root
+  end
+
+  def test_tree_insert_returns_tree_depth
+    tree = BinarySearchTree.new
+
+    assert_nil tree.root
+    assert_equal 0, tree.insert(98, "Animals United")
+    assert_equal 1, tree.insert(36, "Bill & Ted's Bogus Journey")
+    refute_nil tree.root
+
+  end
+
+
+  def test_includes
+    tree = BinarySearchTree.new
+    tree.insert(98, "Animals United")
+    tree.insert(58, "Armageddon")
+    tree.insert(36, "Bill & Ted's Bogus Journey")
+    tree.insert(93, "Bill & Ted's Excellent Adventure")
+    tree.insert(86, "Charlie's Angels")
+    tree.insert(38, "Charlie's Country")
+    tree.insert(69, "Collateral Damage")
+
+    assert tree.include?(69)
+    assert tree.include?(93)
+    refute tree.include?(35)
+    assert tree.include?(36)
+    assert tree.include?(38)
+    assert tree.include?(98)
+    refute tree.include?(45)
+  end
+
+  def test_depth_of_reports_depth
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    tree.insert(16, "Johnny English")
+    tree.insert(92, "Sharknado 3")
+    tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal 1, tree.depth_of(92)
+    assert_equal 2, tree.depth_of(50)
+  end
+
+  def test_max
+    tree = prefilled_tree
+
+    assert_equal ({"Johnny English" => 16}), tree.min
+  end
+
+  def test_min
+    tree = prefilled_tree
+
+    assert_equal ({"Johnny English" => 16}), tree.min
+  end
+
+
+  def test_sort
+    tree = prefilled_tree
+    assert_equal [
+      {"Johnny English"=>16},
+      {"Hannibal Buress: Animal Furnace"=>50},
+      {"Bill & Ted's Excellent Adventure"=>61},
+      {"Sharknado 3"=>92}
+    ] , tree.sort
+  end
+
+  def test_load
+    tree = BinarySearchTree.new
+    assert_equal 99, tree.load('movies.txt')
+  end
+
+  def test_health
+    tree = BinarySearchTree.new
+    tree.insert(98, "Animals United")
+    tree.insert(58, "Armageddon")
+    tree.insert(36, "Bill & Ted's Bogus Journey")
+    tree.insert(93, "Bill & Ted's Excellent Adventure")
+    tree.insert(86, "Charlie's Angels")
+    tree.insert(38, "Charlie's Country")
+    tree.insert(69, "Collateral Damage")
+
+    assert_equal [[98, 7, 100]], tree.health(0)
+    assert_equal [[58, 6, 85]], tree.health(1)
+    assert_equal [[36, 2, 28], [93, 3, 42]], tree.health(2)
+  end
+
+
+end
+
+
+# tree.insert(58, "Armageddon")
+# tree.insert(99, 'Usual Suspects')
